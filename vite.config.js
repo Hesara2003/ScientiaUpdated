2fbedf39c8165ff7e6d94e4e75da6ce9bdffd4e3
Hesara2003 <it23596702@my.sliit.lk>
2025-05-21 09:26:33 +0530

    undo changes

--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
+
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     tailwindcss(),
-    react()
-  ],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://16.171.173.27:8080',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  }
+    react()]
 })
